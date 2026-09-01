2c29a84823f5babb4d72a7781288e2d26dbf6c76
James Prevett <jp@jamespre.dev>
2026-09-01 15:01:22 -0500

    Configure logs and default directories

--- src/init.ts
+++ src/init.ts
@@ -1,12 +1,13 @@
 import { driver_init } from './drivers/base/init.js';
 import { char_dev_init } from './drivers/char/mem.js';
-import { kobj_init } from './index.js';
+import { kobj_init } from './kobject.js';
 import { DevTmpFS } from './fs/devtmpfs.js';
 import { ProcFS } from './fs/procfs.js';
 import { SysFS } from './fs/sysfs.js';
 import { ConfigFS } from './fs/configfs.js';
 import { DebugFS } from './fs/debugfs.js';
 import { configure, InMemory } from '@zenfs/core';
+import { fancy } from 'kerium/log';
 
 export async function init() {
 	kobj_init();
@@ -26,6 +27,13 @@ export async function init() {
 			'/sys/kernel/debug': new DebugFS(),
 			'/tmp': InMemory,
 			'/run': InMemory,
+		},
+		defaultDirectories: true,
+		log: {
+			enabled: true,
+			format: fancy({ colorize: 'message', style: 'process' in globalThis ? 'ansi' : 'css' }),
+			level: 'debug',
+			output: console.log,
 		},
 	});
 }
